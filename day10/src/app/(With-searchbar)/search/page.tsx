8cfe3778448f764518f6ed9b67051caac009f78e
이현빈 <lhb0107@naver.com>
2025-04-03 18:04:09 +0900

    day 10

--- day10/src/app/(With-searchbar)/search/page.tsx
+++ day10/src/app/(With-searchbar)/search/page.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
-export default async function search({searchParams,} : { searchParams: {q?: string}}) {
-    const { q } = await searchParams;
+export default async function search({
+  searchParams,
+}: {
+  searchParams: { q?: string };
+}) {
+  const { q } = await searchParams;
   return <div>search : {q}</div>;
 }
-//   
