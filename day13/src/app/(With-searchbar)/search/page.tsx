d3f91b8282d5b212bd82a1d56c28965b39974272
이현빈 <lhb0107@naver.com>
2025-04-08 11:07:04 +0900

    day13

---
+++ day13/src/app/(With-searchbar)/search/page.tsx
@@ -0,0 +1,27 @@
+import MovieItem from "@/components/movie-Item";
+import React, from "react";
+import style from "./page.module.css";
+import { MovieData } from "@/types";
+
+export default async function Search({searchParams}: {searchParams: {q: string}}) {
+  const { q } = await searchParams;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/search?q=${q}`,
+    { cache: "force-cache" }
+  );
+  // 영화 검색 실행
+  if (!response.ok) {
+    throw new Error("네트워크 응답이 올바르지 않습니다.");
+  }
+  const movies : MovieData[] = await response.json();
+
+  return (
+    <div>
+      <div className={style.container}>
+        {movies.map((movie) => (
+          <MovieItem key={movie.id} {...movie} />
+        ))}
+      </div>
+    </div>
+  );
+}
