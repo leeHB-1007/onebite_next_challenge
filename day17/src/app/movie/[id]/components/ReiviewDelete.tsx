f3bfc2c19a19160edd0fdf12e94990f01d4328d6
이현빈 <lhb0107@naver.com>
2025-04-14 14:59:31 +0900

    operate Review Delete

--- day17/src/app/movie/[id]/components/ReiviewDelete.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from 'react'
-
-export default function ReiviewDelete() {
-  return (
-    <div>ReiviewDelete</div>
-  )
-}
--- day17/src/app/movie/[id]/components/ReviewItem.tsx
+++ day17/src/app/movie/[id]/components/ReviewItem.tsx
@@ -1,5 +1,6 @@
 import { ReviewData } from "@/types";
 import style from "./ReviewItem.module.css";
+import ReviewDelete from "./ReviewDelete";
 
 export default function ReviewItem({
   id,
@@ -17,7 +18,7 @@ export default function ReviewItem({
         </div>
       </div>
       <div className={style.content}>{content}</div>
-      <div className={style.delete_btn}>🗑️ 리뷰 삭제하기</div>
+      <div className={style.delete_btn}><ReviewDelete reviewId={id} movieId={movieId}/></div>
     </div>
   );
 }
